aa02dc917ffb60f6f61c1f7806050982b18b7f09
TzabarDolev <45394343+TzabarDolev@users.noreply.github.com>
2020-02-19 14:26:21 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import cv2
+capture = cv2.VideoCapture('video.mp4')
+frames, image = capture.read()
+count = 0 # counting frames
+k=2000 # read every k frames
+while frames:
+  capture.set(cv2.CAP_PROP_POS_MSEC, (count * k))
+  cv2.imwrite("frames/frame%d.jpg" % count, image)
+  frames,image = capture.read()
+  print ('Read frame number: ', count)
+  count += 1
